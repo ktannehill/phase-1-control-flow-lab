a8ba0137028239327104a4b49fb8a47d82df676f
Kat Tannehill <katetannehill@gmail.com>
2023-09-11 11:53:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ft){
+  if(ft <= 400) {
+    return 'This one is on me!';
+  } else if(ft > 400 && ft <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if(ft > 2000 && ft <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous': 
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
+};
